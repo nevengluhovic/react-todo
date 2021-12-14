e5e5e7c5225cd053b37ae3ff73150d04b3cb1615
Neven Gluhovic <nevengluhovic@gmail.com>
2021-12-14 22:40:02 +0100

    adding todo component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const Form = () => {
+const Form = ({ setInputText, inputText, todos, setTodos }) => {
+  //Getting the input value
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([...todos, { text: inputText, completed: false, id: uuidv4() }]);
+    setInputText("");
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" className="todo-input" />
+      <form onSubmit={submitTodoHandler}>
+        <input
+          value={inputText}
+          onChange={inputTextHandler}
+          type="text"
+          className="todo-input"
+        />
         <button className="todo-button" type="submit">
           <i className="fas fa-plus-square"></i>
         </button>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Todo from "./Todo";
 
 const TodoList = () => {
-  return <div></div>;
+  return (
+    <div className="todo-container">
+      <ul className="todo-list"></ul>
+    </div>
+  );
 };
 
 export default TodoList;
