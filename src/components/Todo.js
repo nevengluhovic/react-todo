aa4dadd6e513b8d585f947ecc769f84d49e107cd
Neven Gluhovic <nevengluhovic@gmail.com>
2021-12-14 23:29:13 +0100

    implementing check and delete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,31 @@
 import React from "react";
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, setTodos, todos }) => {
+  const deleteTodoHandler = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+  };
+
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return { ...item, complete: !item.complete };
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+
   return (
     <div className="todo">
-      <li className="todo-item">{todo.text}</li>
-      <button className="complete-btn">
+      <li className={`todo-item ${todo.complete ? "completed" : ""}`}>
+        {todo.text}
+      </li>
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
-      <button className="trash-btn">
+      <button onClick={deleteTodoHandler} className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Todo from "./Todo";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, setTodos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
         {todos.map((todo) => (
-          <Todo todo={todo} />
+          <Todo setTodos={setTodos} todos={todos} todo={todo} key={todo.id} />
         ))}
       </ul>
     </div>
